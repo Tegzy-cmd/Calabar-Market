947d67efbc2725375fcdccc51508e10032d052db
Omoraka Benjamin <omorakabenjamin3@gmail.com>
2025-09-23 23:30:59 +0000

    remove this element <div class="group peer hidden md:block text-sidebar-

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
-import { AdminSidebar } from './_components/admin-sidebar';
 
 export default function AdminLayout({
   children,
@@ -8,17 +6,10 @@ export default function AdminLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <div className="min-h-screen">
-        <Sidebar>
-          <AdminSidebar />
-        </Sidebar>
-        <SidebarInset>
-          <div className="p-4 sm:p-6 lg:p-8">
-            {children}
-          </div>
-        </SidebarInset>
+    <div className="min-h-screen">
+      <div className="p-4 sm:p-6 lg:p-8">
+        {children}
       </div>
-    </SidebarProvider>
+    </div>
   )
 }
